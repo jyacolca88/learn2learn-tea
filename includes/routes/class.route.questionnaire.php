3a6939f2c68aab068d7940d0327ddabcfff36721
Johnny Yacolca <j.yacolca@westernsydney.edu.au>
2022-07-28 09:04:17 +1000

    changed the route regex to accept commas as well

--- includes/routes/class.route.questionnaire.php
+++ includes/routes/class.route.questionnaire.php
@@ -40,7 +40,7 @@ class Questionnaire_Learn2Learn_Custom_Route extends WP_REST_Controller {
 
         ));
 
-        register_rest_route ( $namespace, $resource_name . '/topics/(?P<topic_ids>+)', array(
+        register_rest_route ( $namespace, $resource_name . '/topics/(?P<topic_ids>[\w\-],?)+)', array(
 
             array(
                 'methods'               => WP_REST_Server::READABLE,
