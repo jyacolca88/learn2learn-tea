6ce6798ab4ea262bd51a9d4b3b39fa79fffe8f29
Johnny Yacolca <j.yacolca@westernsydney.edu.au>
2022-07-27 15:15:50 +1000

    Moved PHP classes into its own folder

--- includes/include.classes.php
+++ includes/include.classes.php
@@ -1,6 +1,6 @@
 <?php
-require_once get_template_directory() . '/includes/class.database.php';
-require_once get_template_directory() . '/includes/class.content_items.php';
-require_once get_template_directory() . '/includes/class.lessons.php';
-require_once get_template_directory() . '/includes/class.questionnaire.php';
-require_once get_template_directory() . '/includes/class.thumbs.php';
+require_once get_template_directory() . '/includes/classes/class.database.php';
+require_once get_template_directory() . '/includes/classes/class.content_items.php';
+require_once get_template_directory() . '/includes/classes/class.lessons.php';
+require_once get_template_directory() . '/includes/classes/class.questionnaire.php';
+require_once get_template_directory() . '/includes/classes/class.thumbs.php';
