824c7246f39624653cb69bb196968206fb010177
Johnny Yacolca <j.yacolca@westernsydney.edu.au>
2022-07-28 14:49:37 +1000

    testing route for all topics

--- includes/routes/class.route.questionnaire.php
+++ includes/routes/class.route.questionnaire.php
@@ -135,6 +135,7 @@ class Questionnaire_Learn2Learn_Custom_Route extends WP_REST_Controller {
         } else {
 
             $topics = Learn2Learn_Topics::get_all_topics();
+            $topics = "is this working?";
 
         }
 
