3c89b73026691a59d9a7a35da2bf610b680b3b95
Johnny Yacolca <j.yacolca@westernsydney.edu.au>
2022-08-16 14:18:32 +1000

    Added class.route.auth.php still needs work.

---
+++ includes/routes/class.route.auth.php
@@ -0,0 +1,41 @@
+<?php
+
+class Auth_Learn2Learn_Custom_Route extends WP_REST_Controller {
+
+    public function register_routes(){
+
+        $version = '1';
+        $namespace = 'learn2learn/v' . $version;
+        $resource_name = 'auth';
+
+
+        register_rest_route( $namespace, '/' . $resource_name, array(
+
+            array(
+                'methods'               => WP_REST_Server::READABLE,
+                'callback'              => array ( $this, 'authorise'),
+                'permissions_callback'  => array ( $this, 'authorise_permissions_check' ),
+                'args'                  => array ()
+            )
+
+        ));
+
+    }
+
+    public function authorise( $request ){
+
+        // $L2l_Content_Items = new Learn2Learn_Content_Items("85daa4da50ba3931755b1960bf8f1083");
+        // $content_items = $L2l_Content_Items->get_content_items();
+
+        return new WP_REST_Response( "Auth REST END POINT", 200 );
+
+
+    }
+
+    public function authorise_permissions_check( $request ){
+
+        return '__return_true';
+
+    }
+
+}
