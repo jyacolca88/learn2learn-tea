608e1b84a1e049feab00bc06011f41791533c8f0
Johnny Yacolca <j.yacolca@westernsydney.edu.au>
2022-08-18 09:50:10 +1000

    Added condition if jwt_auth_token function does NOT exist, throw exception

--- includes/classes/class.auth.php
+++ includes/classes/class.auth.php
@@ -25,8 +25,7 @@ class Learn2Learn_Auth{
 
     public function authenticate(){
 
-        // if (!function_exists("jwt_auth_token_before_dispatch")) throw new Exception("JWT Authentication for WP REST API not configured");
-        if (true) throw new Exception("JWT Authentication for WP REST API not configured");
+        if (!function_exists("jwt_auth_token_before_dispatch")) throw new Exception("JWT Authentication for WP REST API not configured");
 
         if ($this->error_response) return $this->error_response;
 
