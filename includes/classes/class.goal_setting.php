7947b473472c8f4725e0e67d19f7e7fb874fce2e
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-08-31 08:21:58 +1000

    Testing format_and_santize_user_input function.

--- includes/classes/class.goal_setting.php
+++ includes/classes/class.goal_setting.php
@@ -27,7 +27,14 @@ class Learn2Learn_Goal_Setting extends Learn2Learn_Database {
     // Add new Goal
     public function add_new_goal($goal_data){
 
+        // Format and Sanitize User Input
+        $user_data = $this->format_and_sanitize_user_input($goal_data);
+        return $user_data;
 
+        // Split Steps from Goals (if applicable)
+
+
+        // Insert Goal to DB, return Goal ID
 
     }
 
@@ -56,22 +63,12 @@ class Learn2Learn_Goal_Setting extends Learn2Learn_Database {
             ORDER BY g.goal_id ASC, s.step_order ASC
         ", $this->username);
 
-        /*
-        Select *, 
-        FROM goal_table g
-        WHERE g.user_id = username
-        
-
-        SELECT * 
-        FROM steps_table s
-        WHERE 
-        */
-
         $results = $this->db->get_results($sql);
 
         return $this->optimise_raw_results_into_associative_array($results);
 
     }
+
     private function optimise_raw_results_into_associative_array($results){
 
         if (!is_array($results) || empty($results)) {return;}
@@ -117,15 +114,117 @@ class Learn2Learn_Goal_Setting extends Learn2Learn_Database {
 
     }
 
-    private function get_goal_with_steps($goal_id){
+    /**
+     * 
+     * Split goals and steps from data array
+     * 
+     * @param   array   $data       santitized user input values from form
+     * @return  array   array       associative array of goal and steps
+     * 
+     */
+    private function split_goal_and_steps($sanitized_data){
 
-        $goal_id = intval($goal_id);
+        $goal_data = array();
+        $multiple_steps_data = array();
 
-        $sql = $this->db->prepare(
+        foreach($sanitized_data as $key => $value){
 
+            if (empty($value) && $value != 0)
+                continue;
+
+            if (strpos($key, 'goal_') !== false) {
+
+                if ($key == "goal_title" || $key == "goal_reflection")
+                    $value = $this->encrypt_decrypt_string($value);
+
+                $goal_data[$key] = $value;
+            }
+
+            if ($key == "steps" && is_array($value)){
+
+                foreach($value as $k => $v ){
+
+                    // $value is array
+                    // $k is index of array
+                    // $v then is assoc array of key=>value
+
+                }
+
+            }
+
+            // if (strpos($key, 'step_') !== false) {
+            //     $index = substr($key, -1);
+            //     $key = substr($key, 0, strrpos( $key, '_') );
+
+            //     if ($key == "step_title")
+            //         $value = $this->encrypt_decrypt_string($value);
+
+            //     $multiple_steps_data[$index][$key] = $value;
+            // }
+
+            if ($key == "user_id"){
+                $goal_data[$key] = $value;
+            }
+
+        }
+
+        if (isset($sanitized_data["user_id"])){
+            foreach($multiple_steps_data as $key => $value){
+                $multiple_steps_data[$key]["user_id"] = $sanitized_data["user_id"];
+            }
+        }
+
+        return array(
+            "goal" => $goal_data,
+            "steps" => $multiple_steps_data
         );
 
     }
+
+
+    /**
+     * 
+     * Format user input into database field and value pairs and sanitize the data
+     * 
+     * @param   array   $unsafe_input   raw user input data from form
+     * @return  array   $safe_input     sanitized db field and value pair array
+     * 
+     */
+    private function format_and_sanitize_user_input($unsafe_input){
+
+        if (!is_array($unsafe_input))
+            return;
+
+        $safe_input = array();
+
+        foreach($unsafe_input as $key => $value){
+
+            if (is_array($value)){
+
+                $nested_array = array();
+                $nested_index = 0;
+
+                foreach($value as $k => $v){
+
+                    $nested_array[$nested_index][$k] = sanitize_text_field(trim($v));
+                    $nested_index++;
+
+                }
+
+                $safe_input[$key] = $nested_array;
+
+            } else {
+
+                $safe_input[$key] = sanitize_text_field(trim($value));
+
+            }
+
+        }
+
+        return $safe_input;
+
+    }
+
 
     /**
      * 
@@ -163,6 +262,179 @@ class Learn2Learn_Goal_Setting extends Learn2Learn_Database {
 
     }
 
+    /***********************************************/
+    /********** GOAL DATABASE QUERIES [BEGIN] ******/
+    /***********************************************/
 
+    /******************** INSERT GOAL [BEGIN] ********************/
+
+    private function db_insert_goal($insert_data){
+
+        // Sanitize Data
+        foreach($insert_data as $key => $value){
+            $insert_data[$key] = sanitize_text_field(trim($value));
+        }
+
+        // Data Format
+        $data_format = "%s";
+
+        // Insert recored and return ID
+        if ($this->db->insert( $this->goals_table, $insert_data, $data_format ))
+            return $this->db->insert_id;
+
+    }
+
+    /******************** INSERT GOAL [END] ********************/
+
+    /******************** UPDATE GOAL [BEGIN] ********************/
+
+    private function db_update_goal($update_data){
+
+        $data_format = array();
+
+        foreach($update_data as $key => $value){
+
+            if ($key == "goal_id")
+                continue;
+
+            // Sanitize Data
+            $update_data[$key] = sanitize_text_field(trim($value));
+
+            // Populate Data Format
+            array_push($data_format, "%s");
+
+        }
+
+        $where_clause = array (
+            "goal_id" => intval($update_data["goal_id"])
+        );
+
+        unset($update_data["goal_id"]);
+
+        return $this->db->update( $this->goals_table, $update_data, $where_clause, $data_format );
+
+    }
+
+    /******************** UPDATE GOAL [END] ********************/
+
+    /******************** DELETE GOAL [BEGIN] ********************/
+
+    private function db_delete_goal($id){
+
+        $where_format = "%d";
+
+        $where_clause = array (
+            "goal_id" => intval($id)
+        );
+
+        return $this->db->delete( $this->goals_table, $where_clause, $where_format );
+
+    }
+
+    /******************** DELETE GOAL [END] ********************/
+
+    /*********************************************/
+    /********** GOAL DATABASE QUERIES [END] ******/
+    /*********************************************/
+
+    /***********************************************/
+    /********** STEP DATABASE QUERIES [BEGIN] ******/
+    /***********************************************/
+
+    /******************** INSERT STEP [BEGIN] ********************/
+
+    private function db_insert_step($insert_data){
+
+        $data_format = array();
+
+        foreach($insert_data as $key => $value){
+
+            // Sanitize Data
+            $insert_data[$key] = sanitize_text_field(trim($value));
+
+            // Populate Data Format
+            switch($key){
+
+                case "goal_id":
+                case "step_status":
+                case "step_order":
+                    array_push($data_format, "%d");
+                    break;
+
+                default:
+                    array_push($data_format, "%s");
+                
+            }
+
+        }
+
+        if ($this->db->insert( $this->steps_table, $insert_data, $data_format ))
+            return $this->db->insert_id;
+
+    }
+
+    /******************** INSERT STEP [END] ********************/
+
+    /******************** UPDATE STEP [BEGIN] ********************/
+
+    private function db_update_step($update_data){
+
+        $data_format = array();
+
+        foreach($update_data as $key => $value){
+
+            if ($key == "step_id")
+                continue;
+
+            // Sanitize Data
+            $update_data[$key] = sanitize_text_field(trim($value));
+
+            // Populate Data Format
+            switch($key){
+
+                case "goal_id":
+                case "step_status":
+                case "step_order":
+                    array_push($data_format, "%d");
+                    break;
+
+                default:
+                    array_push($data_format, "%s");
+                
+            }
+
+        }
+
+        $where_clause = array (
+            "step_id" => intval($update_data["step_id"])
+        );
+
+        unset($update_data["step_id"]);
+
+        return $this->db->update( $this->steps_table, $update_data, $where_clause, $data_format );
+
+    }
+
+    /******************** UPDATE STEP [END] ********************/
+
+    /******************** DELETE STEP [BEGIN] ********************/
+
+    private function db_delete_step($id){
+
+        $where_format = "%d";
+
+        $where_clause = array (
+            "step_id" => intval($id)
+        );
+
+        return $this->db->delete( $this->steps_table, $where_clause, $where_format );
+
+    }
+
+    /******************** DELETE STEP [END] ********************/
+
+    /*********************************************/
+    /********** STEP DATABASE QUERIES [END] ******/
+    /*********************************************/
 
 } 
--- includes/routes/class.route.goal_setting.php
+++ includes/routes/class.route.goal_setting.php
@@ -26,7 +26,32 @@ class Goal_Setting_Learn2Learn_Custom_Route extends WP_REST_Controller {
 
         $username = sanitize_text_field($request["username"]);
         $L2l_Goal_Setting = new Learn2Learn_Goal_Setting($username);
-        $goals = $L2l_Goal_Setting->get_goals();
+        // $goals = $L2l_Goal_Setting->get_goals();
+
+        // TESTING ADD goal
+        $new_goal = array(
+            "goal_title" => "New Goal HERE!",
+            "goal_completed_by" => "2022-08-31",
+            "steps" => array(
+                array(
+                    "step_title" => "New Step Here, Step 1",
+                    "step_completed_by" => "2022-08-31",
+                    "step_order" => 0
+                ),
+                array(
+                    "step_title" => "New Step Here, Step 2",
+                    "step_completed_by" => "2022-08-31",
+                    "step_order" => 1
+                ),
+                array(
+                    "step_title" => "New Step Here, Step 3",
+                    "step_completed_by" => "2022-08-31",
+                    "step_order" => 2
+                )
+            )
+        );
+
+        $goals = $L2l_Goal_Setting->add_new_goal($new_goal);
 
         return new WP_REST_Response( $goals, 200 );
 
