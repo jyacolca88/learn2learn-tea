3b7c657dfdd6b983f172d653ac40ee2c66ab0edb
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-08-30 16:00:43 +1000

    Added encrypt and decrypt function.

--- includes/classes/class.goal_setting.php
+++ includes/classes/class.goal_setting.php
@@ -74,27 +74,40 @@ class Learn2Learn_Goal_Setting extends Learn2Learn_Database {
     }
     private function optimise_raw_results_into_associative_array($results){
 
-        if (!$results) {return;}
+        if (!is_array($results) || empty($results)) {return;}
 
         $goals_array = [];
         $steps_array = [];  
 
         foreach($results as $record){
 
-            $steps_array[$record->goal_id][$record->step_order] = array(
-                "step_id" => $record->step_id,
-                "step_title" => $record->step_title,
-                "step_completed_by" => $record->step_completed_by,
-                "step_status" => $record->step_status,
-                "step_order" => $record->step_order
-            );
+            if (isset($record->step_id) && !empty($record->step_id)){
+
+                $step_title = $this->encrypt_decrypt_string($record->step_title, true);
+                $goal_title = $this->encrypt_decrypt_string($record->goal_title, true);
+
+                $steps_array[$record->goal_id][$record->step_order] = array(
+                    "step_id" => $record->step_id,
+                    "step_title" => $step_title,
+                    "step_completed_by" => $record->step_completed_by,
+                    "step_status" => $record->step_status,
+                    "step_order" => $record->step_order
+                );
+
+            } else {
+
+                $steps_array[$record->goal_id] = false;
+
+            }
+            
+            $goal_reflection = (!empty($record->goal_reflection) ? $this->encrypt_decrypt_string($record->goal_reflection, true) : null);
 
             $goals_array[$record->goal_id] = array(
                 "goal_id" => $record->goal_id,
-                "goal_title" => $record->goal_title,
+                "goal_title" => $goal_title,
                 "goal_completed_by" => $record->goal_completed_by,
                 "goal_status" => $record->goal_status,
-                "goal_reflection" => $record->goal_reflection,
+                "goal_reflection" => $goal_reflection,
                 "steps" => $steps_array[$record->goal_id]
             );
 
@@ -113,6 +126,43 @@ class Learn2Learn_Goal_Setting extends Learn2Learn_Database {
         );
 
     }
+
+    /**
+     * 
+     * Encrypt and Descrypt string, returns encrypted or decrypted string
+     * 
+     * @param   string   $string        raw plain text string
+     * @param   bool     $decrypt       optional boolean flag to trigger decryption
+     * @return  string   $output        returns encrypted or decrypted string
+     * 
+     */
+    private function encrypt_decrypt_string( $string, $decrypt = false ) {
+
+        if(empty($string))
+            return $string;
+        
+        $secret_key = 'D355B64824A381B1A944601B05A36BE3801E859D';   // SHA1 of: lfgs
+        $secret_iv = '68A6FFE86D0E72E768168B49D33B1B91B06C08DD';    // SHA1 of: goal_setting
+    
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $key = hash( 'sha256', $secret_key );
+        $iv = substr( hash( 'sha256', $secret_iv ), 0, 16 );
+    
+        if( $decrypt ) {
+            
+            $output = openssl_decrypt( base64_decode( $string ), $encrypt_method, $key, 0, $iv );
+
+        } else {
+            
+            $output = base64_encode( openssl_encrypt( $string, $encrypt_method, $key, 0, $iv ) );
+
+        }
+    
+        return ($output ? $output : $string );
+
+    }
+
 
 
 } 
