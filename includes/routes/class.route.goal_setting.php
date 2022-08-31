4feec3a4a28a2f691f01dbac6c89022b78e68dc7
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-08-31 10:25:49 +1000

    Added new path for POST goal.

--- includes/routes/class.route.goal_setting.php
+++ includes/routes/class.route.goal_setting.php
@@ -20,13 +20,43 @@ class Goal_Setting_Learn2Learn_Custom_Route extends WP_REST_Controller {
 
         ));
 
+        register_rest_route( $namespace, '/' . $resource_name . '/goal/', array(
+
+            array(
+                'methods'               => WP_REST_Server::CREATABLE,
+                'callback'              => array ( $this, 'post_goal'),
+                'permission_callback'  => array ( $this, 'post_goal_permissions_check' ),
+                'args'                  => array ()
+            )
+
+        ));
+
     }
 
     public function get_goals( $request ){
 
         $username = sanitize_text_field($request["username"]);
+
         $L2l_Goal_Setting = new Learn2Learn_Goal_Setting($username);
         $goals = $L2l_Goal_Setting->get_goals();
+
+        return new WP_REST_Response( $goals, 200 );
+
+    }
+
+    public function get_goals_permissions_check( $request ){
+
+        return '__return_true';
+
+    }
+
+    public function post_goal( $request ){
+
+        $post_data = $request->get_params();
+
+        $username = sanitize_text_field($post_data["username"]);
+
+        $L2l_Goal_Setting = new Learn2Learn_Goal_Setting($username);
 
         // TESTING ADD goal
         $new_goal = array(
@@ -51,15 +81,15 @@ class Goal_Setting_Learn2Learn_Custom_Route extends WP_REST_Controller {
             )
         );
 
-        // $goals = $L2l_Goal_Setting->add_new_goal($new_goal);
+        $goal = $L2l_Goal_Setting->add_new_goal($new_goal);
 
-        return new WP_REST_Response( $goals, 200 );
+        return new WP_REST_Response( $goal, 200 );
 
     }
 
-    public function get_goals_permissions_check( $request ){
+    public function post_goal_permissions_check(){
 
-        return '__return_true';
+        return current_user_can( 'read' );
 
     }
 
