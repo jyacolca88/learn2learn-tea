f7896662cbb9c7cd3ecabe2f1bf798c296224a4b
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-09-06 16:07:30 +1000

    Added add_new_user_progress_by_page_id method.

--- includes/classes/class.userprogress.php
+++ includes/classes/class.userprogress.php
@@ -9,11 +9,40 @@ class Learn2Learn_Userprogress extends Learn2Learn_Database {
 
         $this->username = $username;
 
+        date_default_timezone_set('Australia/Sydney');
+
     }
 
     public function get_user_progress_by_page_id($page_id){
 
         return $this->select_user_progress_record($this->username, $page_id);
+
+    }
+
+    public function add_new_user_progress_by_page_id($content_id){
+
+        $exists = $this->select_user_progress_record($this->username, $content_id);
+        if($exists){ return; } 
+
+        $user_id = $this->username;
+        $content_id = intval($content_id);
+        $page_id = intval(wp_get_post_parent_id($content_id));
+        $progress = 1;
+        $time = current_time('mysql');
+
+        $table_data = array (
+            'user_id' => $user_id,
+            'content_id' => $content_id,
+            'page_id' => $page_id,
+            'progress' => $progress,
+            'time' => $time
+        );
+
+        $data_format = array('%s', '%d', '%d', '%d', '%s');
+
+        $success = $this->db->insert($this->content_progress_table, $table_data, $data_format);
+
+        return ($success ? true : false);
 
     }
 
