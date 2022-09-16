5c68b3f606bde7a96a73fbb521de4ab81ac8917a
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-09-16 12:31:51 +1000

    Testing Class for menu options. Testing.

---
+++ includes/menus/class.menu.options.php
@@ -0,0 +1,39 @@
+<?php
+class Learn2Learn_Menu_Options {
+
+    protected $page_title;
+    protected $page_link_text;
+    protected $user_capabilities;
+    protected $page_slug = "l2l-options";
+    protected $page_link_icon;
+
+    function __construct() {
+
+        $this->page_title = "Learn2Learn Options Menu";
+        $this->page_link_text = "L2L Options";
+        $this->user_capabilities = "manage_options";
+        $this->page_link_icon = "dashicons-lightbulb";
+
+    }
+
+    private function add_menu_page(){
+
+        add_menu_page(
+            $this->page_title,
+            $this->page_link_text,
+            $this->user_capabilities,
+            $this->page_slug,
+            $this->render_page,
+            $this->$page_link_icon,
+            3
+        );
+
+    }
+
+    private function render_page(){
+
+        require_once get_template_directory() . '/includes/menus/menu.options.content.php';
+
+    }
+
+}
--- includes/menus/menu.options.php
+++ includes/menus/menu.options.php
@@ -3,16 +3,21 @@
 add_action( 'admin_menu', 'lf_l2l_tea_options_menu' );
  
 function lf_l2l_tea_options_menu(){
- 
-	add_menu_page(
-		'Learn2Learn Options Menu', // page <title>Title</title>
-		'L2L Options', // link text
-		'manage_options', // user capabilities
-		'l2l-options', // page slug
-		'lf_l2l_tea_options_menu_callback', // this function prints the page content
-		'dashicons-lightbulb', // icon (from Dashicons for example)
-		3 // menu position
-	);
+
+    require_once get_template_directory() . '/includes/menus/class.options.content.php';
+    $L2L_Menu_Options = new Learn2Learn_Menu_Options();
+    $L2L_Menu_Options->add_menu_page();
+
+
+	// add_menu_page(
+	// 	'Learn2Learn Options Menu', // page <title>Title</title>
+	// 	'L2L Options', // link text
+	// 	'manage_options', // user capabilities
+	// 	'l2l-options', // page slug
+	// 	'lf_l2l_tea_options_menu_callback', // this function prints the page content
+	// 	'dashicons-lightbulb', // icon (from Dashicons for example)
+	// 	3 // menu position
+	// );
 }
  
 function lf_l2l_tea_options_menu_callback(){
