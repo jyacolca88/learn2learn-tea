029718b1c18dcac3e9174f12a226a843110167bc
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-09-20 10:07:59 +1000

    Added class.settings.fields and commented out procedural adding sections code.

--- includes/menus/class.settings.fields.php
+++ includes/menus/class.settings.fields.php
@@ -3,10 +3,111 @@ class Learn2Learn_Settings_Fields extends Learn2Learn_Menu_Options {
 
     private $options_group;
     private $section_id;
+    private $section_title;
+    private $field_id;
+    private $field_title;
+    private $field_args;
 
     function __construct() {
 
         $this->options_group = $this->page_slug . "-settings";
+
+    }
+
+    public function add_section($section_id, $section_title){
+
+        $this->section_id = sanitize_text_field($section_id);
+        $this->section_title = sanitize_text_field($section_title);
+        add_settings_section($this->section_id, $this->section_title, '', $this->page_slug);
+
+    }
+
+    public function register_and_add_field($field_id, $field_title, $field_type="text", $field_args = array()){
+
+        $this->field_id = sanitize_text_field($field_id);
+        $this->field_title = sanitize_text_field($field_title);
+        $this->field_args = array(
+            "label_for" => $this->field_id,
+            "name" => $this->field_id
+        );
+
+        if (!empty($field_args))
+            array_merge($this->field_args, $field_args);
+
+        switch($field_type){
+
+            case "text":
+                $sanitize_method = "sanitize_text_field";
+                $render_method = "render_text_field";
+                break;
+
+            case "textarea":
+                $sanitize_method = "sanitize_textarea_field";
+                $render_method = "render_textarea_field";
+                break;
+
+        }
+
+
+        register_setting($this->options_group, $this->field_id, array($this, $sanitize_method));
+
+        add_settings_field($this->field_id, $this->field_title, array($this, $render_method), $this->page_slug, $this->section_id, $this->field_args);
+
+    }
+
+    public function render_text_field($args){
+
+        $attributes_array = $this->retrieve_values_from_args($args);
+        extract($attributes_array);
+
+        printf(
+            "<input type='text' id='%s' name='%s' value='%s' style='%s' />",
+            $name,
+            $name,
+            $value,
+            $style
+        );
+
+    }
+
+    public function render_textarea_field($args){
+
+        $attributes_array = $this->retrieve_values_from_args($args);
+        extract($attributes_array);
+
+        printf(
+            "<textarea id='%s' name='%s' style='%s'>%s</textarea>",
+            $name,
+            $name,
+            $style,
+            $value
+        );
+
+    }
+
+    private function retrieve_values_from_args($args){
+
+        $name = (isset($args["name"]) ? sanitize_text_field($args["name"]) : "");
+        $value = (get_option($name) !== null ? sanitize_text_field(get_option($name)) : "");
+        $style = (isset($args["name"]) ? esc_attr(($args["name"])) : "");
+
+        return array(
+            "name" => $name,
+            "value" => $value,
+            "style" => $style
+        );
+
+    }
+
+    public function sanitize_text_field($value){
+
+        return sanitize_text_field((trim($value)));
+
+    }
+
+    public function sanitize_textarea_field($value){
+
+        return strval((trim($value)));
 
     }
 
--- includes/menus/menu.options.php
+++ includes/menus/menu.options.php
@@ -7,17 +7,6 @@ function lf_l2l_tea_options_menu(){
     require_once get_template_directory() . '/includes/menus/class.menu.options.php';
     $L2L_Menu_Options = new Learn2Learn_Menu_Options();
     $L2L_Menu_Options->add_menu_page();
-
-
-	// add_menu_page(
-	// 	'Learn2Learn Options Menu', // page <title>Title</title>
-	// 	'L2L Options', // link text
-	// 	'manage_options', // user capabilities
-	// 	'l2l-options', // page slug
-	// 	'lf_l2l_tea_options_menu_callback', // this function prints the page content
-	// 	'dashicons-lightbulb', // icon (from Dashicons for example)
-	// 	3 // menu position
-	// );
 }
  
 function lf_l2l_tea_options_menu_callback(){
@@ -26,88 +15,90 @@ function lf_l2l_tea_options_menu_callback(){
 
 add_action( 'admin_init',  'lf_l2l_settings_fields' );
 
-function lf_l2l_settings_fields(){
 
-	// I created variables to make the things clearer
-	$page_slug = 'l2l-options';
-	$option_group = 'l2l-options-settings';
-    $section_id = 'l2l_general_section';
-    $main_heading_name = 'l2l-main-heading';
-    $main_paragraph_name = 'l2l-main-paragraph';
 
-	// 1. create section
-	add_settings_section(
-		$section_id , // section ID
-		'General Settings', // title (optional)
-		'', // callback function to display text after title but before fields (optional)
-		$page_slug
-	);
+// function lf_l2l_settings_fields(){
 
-	// 2. register fields
-	register_setting( $option_group, $main_heading_name, array("type" => "string", "sanitize_callback" => "l2l_options_main_heading_sanitize") );
-	register_setting( $option_group, $main_paragraph_name, array("type" => "string", "sanitize_callback" => "l2l_options_main_paragraph_sanitize") );
+// 	// I created variables to make the things clearer
+// 	$page_slug = 'l2l-options';
+// 	$option_group = 'l2l-options-settings';
+//     $section_id = 'l2l_general_section';
+//     $main_heading_name = 'l2l-main-heading';
+//     $main_paragraph_name = 'l2l-main-paragraph';
 
-	// 3. add fields
-	add_settings_field(
-		$main_heading_name,
-		'Main Heading',
-		'l2l_options_main_heading_textbox', // function to print the field
-		$page_slug,
-		$section_id,  // section ID
-        array(
-            'label_for' => $main_heading_name, 
-            'name' => $main_heading_name,
-            'style' => "width:22rem; max-width:100%"
-        )
-	);
+// 	// 1. create section
+// 	add_settings_section(
+// 		$section_id , // section ID
+// 		'General Settings', // title (optional)
+// 		'', // callback function to display text after title but before fields (optional)
+// 		$page_slug
+// 	);
 
-	add_settings_field(
-		$main_paragraph_name,
-		'Main Paragraph',
-		'l2l_options_main_paragraph_textarea',
-		$page_slug,
-		$section_id,
-		array(
-			'label_for' => $main_paragraph_name,
-			'name' => $main_paragraph_name, // pass any custom parameters
-            'style' => "width: 22rem; max-width:100%; height: 5rem; min-height: 5rem; max-height: 5rem;"
-		)
-	);
+// 	// 2. register fields
+// 	register_setting( $option_group, $main_heading_name, array("type" => "string", "sanitize_callback" => "l2l_options_main_heading_sanitize") );
+// 	register_setting( $option_group, $main_paragraph_name, array("type" => "string", "sanitize_callback" => "l2l_options_main_paragraph_sanitize") );
 
-}
+// 	// 3. add fields
+// 	add_settings_field(
+// 		$main_heading_name,
+// 		'Main Heading',
+// 		'l2l_options_main_heading_textbox', // function to print the field
+// 		$page_slug,
+// 		$section_id,  // section ID
+//         array(
+//             'label_for' => $main_heading_name, 
+//             'name' => $main_heading_name,
+//             'style' => "width:22rem; max-width:100%"
+//         )
+// 	);
 
-// custom callback function to print checkbox field HTML
-function l2l_options_main_heading_textbox( $args ) {
-    $name = $args['name'];
-    $value = get_option($name, '');
-    $style = $args['style'];
-    printf(
-        "<input type='text' id='%s' name='%s' value='%s' style='%s' />",
-        $name,
-        $name,
-        $value,
-        $style
-    );
-}
+// 	add_settings_field(
+// 		$main_paragraph_name,
+// 		'Main Paragraph',
+// 		'l2l_options_main_paragraph_textarea',
+// 		$page_slug,
+// 		$section_id,
+// 		array(
+// 			'label_for' => $main_paragraph_name,
+// 			'name' => $main_paragraph_name, // pass any custom parameters
+//             'style' => "width: 22rem; max-width:100%; height: 5rem; min-height: 5rem; max-height: 5rem;"
+// 		)
+// 	);
 
-function l2l_options_main_paragraph_textarea( $args ) {
-    $name = $args['name'];
-    $value = get_option($name, '');
-    $style = $args['style'];
-    printf(
-        "<textarea id='%s' name='%s' style='%s'>%s</textarea>",
-        $name,
-        $name,
-        $style,
-        $value
-    );
-}
+// }
 
-// custom sanitization function for a checkbox field
-function l2l_options_main_heading_sanitize( $value ) {
-	return sanitize_text_field((trim($value)));
-}
+// // custom callback function to print checkbox field HTML
+// function l2l_options_main_heading_textbox( $args ) {
+//     $name = $args['name'];
+//     $value = get_option($name, '');
+//     $style = $args['style'];
+//     printf(
+//         "<input type='text' id='%s' name='%s' value='%s' style='%s' />",
+//         $name,
+//         $name,
+//         $value,
+//         $style
+//     );
+// }
 
-function l2l_options_main_paragraph_sanitize( $value ) {
-	return strval((trim($value)));
-}
+// function l2l_options_main_paragraph_textarea( $args ) {
+//     $name = $args['name'];
+//     $value = get_option($name, '');
+//     $style = $args['style'];
+//     printf(
+//         "<textarea id='%s' name='%s' style='%s'>%s</textarea>",
+//         $name,
+//         $name,
+//         $style,
+//         $value
+//     );
+// }
+
+// // custom sanitization function for a checkbox field
+// function l2l_options_main_heading_sanitize( $value ) {
+// 	return sanitize_text_field((trim($value)));
+// }
+
+// function l2l_options_main_paragraph_sanitize( $value ) {
+// 	return strval((trim($value)));
+// }
