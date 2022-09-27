cb7870b71947a9ae1059b143b37f4a64f9a4697b
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-09-27 15:09:17 +1000

    returning questionnaire already launched with custom options.

--- includes/routes/class.route.l2l_options.php
+++ includes/routes/class.route.l2l_options.php
@@ -9,7 +9,7 @@ class Learn2Learn_Options_Custom_Route extends WP_REST_Controller {
         $resource_name = 'options';
 
 
-        register_rest_route( $namespace, '/' . $resource_name, array(
+        register_rest_route( $namespace, '/' . $resource_name . "/(?P<user_id>[\d]+)", array(
 
             array(
                 'methods'               => WP_REST_Server::READABLE,
@@ -24,8 +24,15 @@ class Learn2Learn_Options_Custom_Route extends WP_REST_Controller {
 
     public function get_options( $request ){
 
+        $user_id = intval($request["user_id"]);
+
         $results = $this->get_results_from_options_table();
         $fields = $this->format_options_results_into_assoc_array($results);
+        $questionnaire_already_launched = $this->get_questionnaire_already_launched($user_id);
+
+        if (is_array($fields)){
+            $fields["questionnaire_already_launched"] = $questionnaire_already_launched;
+        }
 
         return new WP_REST_Response( $fields, 200 );
 
@@ -71,6 +78,24 @@ class Learn2Learn_Options_Custom_Route extends WP_REST_Controller {
     public function get_options_permissions_check( $request ){
 
         return current_user_can( 'read' );
+
+    }
+
+    private function get_questionnaire_already_launched($user_id){
+
+        return get_user_meta( $user_id, 'l2l_quesitonnaire_already_launched' , true );
+
+    }
+
+    public function set_questionnaire_already_launched($user_id){
+
+        $already_launched = $this->get_questionnaire_already_launched($user_id);
+
+        if (!$already_launched){
+
+            add_user_meta( $user_id, 'l2l_quesitonnaire_already_launched', true, true );
+
+        }
 
     }
 
