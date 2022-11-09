dd9a6b75de2ae54327a7c1db96809e175412051e
Johnny Yacolca <30022647@westernsydney.edu.au>
2022-11-09 12:01:44 +1100

    returning a reset if user_thumbs_records is array.

--- includes/classes/class.database.php
+++ includes/classes/class.database.php
@@ -254,7 +254,9 @@ class Learn2Learn_Database {
         if ($user_id){ $username = $this->get_username_by_user_id($user_id); }
 
         //$table = null, $where_format = array(), $where_values = array(), $columns = null, $array_output = false
-        return $this->select_from_table($this->thumbs_table, array("user_id" => "%s", "page_id" => "%d"), array($username, $lesson_id), array("thumbs"));
+        $user_thumbs_records = $this->select_from_table($this->thumbs_table, array("user_id" => "%s", "page_id" => "%d"), array($username, $lesson_id), array("thumbs"));
+
+        return (is_array($user_thumbs_records) ? reset($user_thumbs_records) : $user_thumbs_records);
 
     }
 
